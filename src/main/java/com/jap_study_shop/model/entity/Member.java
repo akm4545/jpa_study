6a904c01b5cb3e067b2bf881682ec8911be458aa
akm4545 <akm4545@naver.com>
2022-06-17 22:59:57 +0900

    연관관계 공부

--- src/main/java/com/jap_study_shop/model/entity/Member.java
+++ src/main/java/com/jap_study_shop/model/entity/Member.java
@@ -1,9 +1,13 @@
 package com.jap_study_shop.model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Member {
@@ -20,6 +24,9 @@ public class Member {
 	private String street;
 	
 	private String zipcode;
+	
+	@OneToMany(mappedBy = "member")
+	private List<Order> orders = new ArrayList<Order>();
 
 	public Long getId() {
 		return id;
@@ -59,5 +66,13 @@ public class Member {
 
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 }
--- src/main/java/com/jap_study_shop/model/entity/Order.java
+++ src/main/java/com/jap_study_shop/model/entity/Order.java
@@ -1,6 +1,8 @@
 package com.jap_study_shop.model.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,9 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -21,8 +26,18 @@ public class Order {
 	@Column(name = "ORDER_ID")
 	private Long id;
 	
-	@Column(name = "MEMBER_ID")
-	private Long memberId;
+	//연관관계의 주인이 아님 - 외래키 소유 X
+	//제네릭 타입 오더 아이템을 찾은 뒤 
+	//대상 테이블과 오더 필드를 찾고 
+	//조인 컬럼을 확인 후 다시 돌아와 Order에 선언된 컬럼 아이디를 기준으로 쿼리를 생성하여 가져오는듯함
+	@OneToMany(mappedBy = "order") // 필드 이름
+	private List<OrderItem> orderItems = new ArrayList<OrderItem>();
+	
+	//연관관계의 주인 - 외래키 소유 O
+	//Get Member 시 member_id로 조인하여 가져옴
+	@ManyToOne
+	@JoinColumn(name = "MEMBER_ID") //컬럼 이름
+	private Member member;
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date orderDate;
@@ -37,15 +52,7 @@ public class Order {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	public Long getMemberId() {
-		return memberId;
-	}
-
-	public void setMemberId(Long memberId) {
-		this.memberId = memberId;
-	}
-
+	
 	public Date getOrderDate() {
 		return orderDate;
 	}
@@ -60,6 +67,41 @@ public class Order {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(List<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+	
+	//오더 1이 회원 1에 추가되어 있을 시 
+	//오더 1이 회원 2로 변경되었을때 회원 1에 남아있는
+	//오더 1의 관계를 끊기 위함
+	//이렇게 바꾸는 상황이 자주 있을지는 의문
+	public void setMember(Member member) {
+		
+		if(this.member != null) {
+			this.member.getOrders().remove(this);
+		}
+		
+		this.member = member;
+		member.getOrders().add(this);
+	}
+	
+	//오더 아이템을 추가시키면 
+	//오더에서 오더 아이템 리스트에 저장 
+	//추가된 오더 아이템에도 오더를 저장
+	//양방향 연관관계 성립
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
 	}
 }
 
